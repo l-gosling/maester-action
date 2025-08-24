53a4c56f0e4495b2010ca35fbb80ff27dd55b105
Lukas Gosling <71852244+l-gosling@users.noreply.github.com>
2025-08-24 21:03:53 +0200

    test

--- script/Run-MaesterAction.ps1
+++ script/Run-MaesterAction.ps1
@@ -287,6 +287,9 @@ PROCESS {
         # Run Maester tests
         Write-Host "🕑 Start test execution $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
         #$results = Invoke-Maester @MaesterParameters
+        Write-Host $MaesterParameters
+        Write-Host $MaesterParameters.Keys
+        Write-Host $MaesterParameters.Values
         $results = Invoke-Maester -Tag "ORCA.236","ORCA.235","MT.1068"
         Write-Host "🕑 Maester tests executed $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
     } catch {
